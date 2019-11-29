c90ec87d23df424d6d4961715c8cd5208071a7e6
xRemixApplix <r3m1.appli@gmail.com>
2019-11-29 18:41:48 +0100

    Première version : Passage en bois 2 -> Construction de 2 archer et 2 chevaliers et retour position initiale

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 """
 
     Auteur: xR3m1x
-    Date : 17 Novembre 2019
+    Date : 29 Novembre 2019
     Version du programme : 1.0
         Entrainement d'unités pour attaquer basiquement la reine.
 
@@ -34,14 +34,21 @@ class Unite(Entite):
 ## Classe représentant les sites (site vierge ou batiment)
 class Site(Entite):
     # Fonction d'initialisation de la classe 'Site'
-    def __init__(self, id, x, y, radius, typeSite, cycleTrain, ignore1, ignore2) :
+    def __init__(self, id, x, y, owner, radius, typeSite, cycleTrain, typeArmy, ignore1, ignore2) :
         self.id = id                    # Identifiant du site
-        super().__init__(x, y, -1)
-        self.typeSite = typeSite        # Type du site (-1: vide, 0: Chevalier, 1: Archer)
+        super().__init__(x, y, owner)
+        self.typeSite = typeSite        # Type du site (-1: Vide, 2: Caserne)
         self.radius = radius            # Rayon du site
         self.cycleTrain = cycleTrain    # Tour restant avant fin entrainement en cours
+        self.typeArmy = typeArmy        # Type d'unite produite (-1: Vide, 0: Chevalier, 1: Archer)
         self.ignore1 = ignore1          # Ignore pour le moment
         self.ignore2 = ignore2          # Ignore pour le moment
+    # Fonction mettant à jour les informations
+    def maj_site(self, typeSite, owner, cycleTrain, typeArmy):
+        self.typeSite = typeSite
+        self.owner = owner
+        self.cycleTrain = cycleTrain
+        self.typeArmy = typeArmy
 
 ## Classe représentant les elements globaux du jeu
 class Challenge :
@@ -84,7 +91,9 @@ for i in range(challenge.nbSites):
     site_id, x, y, radius = [int(j) for j in input().split()]
 
 #--------------------------------------------------
-    challenge.site.append(Site(site_id, x, y, radius,-1,-1,1,1))
+    challenge.site.append(Site(site_id, x, y, VIDE, radius,VIDE, VIDE, VIDE,VIDE, VIDE))
+    # Position initiale de la reine
+    startX = startY = -1
 #--------------------------------------------------
 
 # Tour de jeu
@@ -93,13 +102,20 @@ while True:
 
 #--------------------------------------------------
     challenge.set_po(gold)
+    nbKnight = 0
+    nbArcher = 0
 #--------------------------------------------------
 
     for i in range(challenge.nbSites):
         site_id, ignore_1, ignore_2, structure_type, owner, param_1, param_2 = [int(j) for j in input().split()]
 
 #--------------------------------------------------
-        
+        challenge.site[site_id].maj_site(structure_type, owner, param_1, param_2)
+        if owner == ALLIE :
+            if param_2 == CHEVALIER :
+                nbKnight += 1
+            elif param_2 == ARCHER :
+                nbArcher +=1
 #--------------------------------------------------
 
     num_units = int(input())
@@ -114,8 +130,38 @@ while True:
         
 #--------------------------------------------------
         challenge.units.append(Unite(x, y, owner, health, unit_type))
+        if unit_type == REINE and owner == ALLIE:
+            if startX == -1 :
+                startX = x
+                startY = y
+            
+            queen = {'xQueen': x, 'yQueen': y}
 #--------------------------------------------------
+
+#-------------------------------------------------#
+#-------------------------------------------------#
+#####           CALCUL DES ACTIONS            #####
+#-------------------------------------------------#
+#-------------------------------------------------#
+    # Parmi la liste de site
+    closestSite = {'idClosest': -1, 'dist': 10000}
+    # RaZ de la liste des sites disponibles pour l'entrainement
+    listTrain = []
+    for i in challenge.site :
+        # Recherche du site vierge le plus près
+        if closestSite['dist'] > math.sqrt((i.x-queen['xQueen'])**2+(i.y-queen['yQueen'])**2) and i.typeSite == VIDE:
+            closestSite['idClosest'] = i.id
+            closestSite['dist'] = math.sqrt((i.x-queen['xQueen'])**2+(i.y-queen['yQueen'])**2)
+        # Recupération des sites disponibles à l'entrainement
+        if i.cycleTrain==0 and i.owner==0:
+            listTrain.append(i.id)
+
     # Première ligne : Action de la reine
     # Seconde ligne : Instructions d'entrainement
-    print("WAIT")
-    print("TRAIN")
+    if nbArcher + nbKnight >= 4 :
+        print("MOVE {} {}".format(startX, startY))
+    elif nbKnight >= nbArcher :
+        print("BUILD {} BARRACKS-ARCHER".format(closestSite['idClosest']))
+    else :
+        print("BUILD {} BARRACKS-KNIGHT".format(closestSite['idClosest']))
+    print("TRAIN",*listTrain,sep=' ')
